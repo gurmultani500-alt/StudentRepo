ac367e5bc161f8e20f12324cab106208aa6824f2
gurmultani500-alt <gurmultani500@gmail.com>
2026-01-20 12:06:50 +0000

    Format Lab01 Java file and add author comment

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -5,7 +5,7 @@ void testMethod1()
   //First command x = y + z
   //Second commnad print(x)
 
-
+// GurpreetKaur_230213207
   //Last line print('Good bye')
 }
 
@@ -21,3 +21,4 @@ void main()
     testMethod1();
     testMethod2();
 }
+
